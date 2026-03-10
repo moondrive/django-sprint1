31840d7bda1c1d24255075eb222af01fa41905be
moondrive <vasily-filimon@yandex.ru>
2026-03-10 15:11:58 +0300

    work

---
+++ blogicum/pages/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+app_name = 'pages'
+urlpatterns = [
+    path('about/', views.about, name='about'),
+    path('rules/', views.rules, name='rules'),
+]
